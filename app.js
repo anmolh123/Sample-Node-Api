52c11463df6e0c46e0eb38e760ca371530dddd94
anmolh123 <hanmol123@outlook.com>
2018-02-24 19:16:42 +0530

    route added

--- app.js
+++ app.js
@@ -32,9 +32,11 @@ app.use((req, res, next) => {
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
